0894733fbec2b1cf3842d80194309289db752a01
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2025-01-25 22:41:42 +0700

    ada security config

--- src/main/java/org/zan/demo/controller/auth/AuthController.java
+++ src/main/java/org/zan/demo/controller/auth/AuthController.java
@@ -1,9 +1,15 @@
 package org.zan.demo.controller.auth;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.zan.demo.data.dto.CommonResponseDTO;
+import org.zan.demo.data.dto.request.AuthRequestDTO;
+import org.zan.demo.data.dto.response.AuthResponseDto;
+import org.zan.demo.entity.User;
+import org.zan.demo.service.impl.AuthenticationService;
+import org.zan.demo.util.TokenService;
 
 @CrossOrigin("*")
 @RestController
@@ -11,4 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthenticationService authenticationService;
+    private final TokenService tokenService;
+
+    @PostMapping("/register")
+    public ResponseEntity<CommonResponseDTO<User>> registerUser(@RequestBody AuthRequestDTO body){
+        User user = authenticationService.registerUser(body.getUsername(), body.getPassword());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponseDTO.<User>builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .message("Success register new user")
+                        .data(user).build()
+                );
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @param body The login credentials.
+     * @return A response containing login information.
+     */
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponseDTO<AuthResponseDto>> loginUser(@RequestBody AuthRequestDTO body){
+        AuthResponseDto authResponseDto = authenticationService.loginUser(body.getUsername(), body.getPassword());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponseDTO.<AuthResponseDto>builder()
+                        .statusCode(HttpStatus.CREATED.value())
+                        .message("Success login")
+                        .data(authResponseDto).build()
+                );
+    }
 }
